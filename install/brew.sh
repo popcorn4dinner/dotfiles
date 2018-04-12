472fe13c01674cc0e25ebc6177fefe3e911d6f79
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-12 13:37:34 +0200

    squash! work in progress

--- install/brew.sh
+++ install/brew.sh
@@ -1,4 +1,4 @@
-if ! [[ -x "/usr/local/bin/brew" ]] then
+if ! [ -x "/usr/local/bin/brew" ] then
     echo "Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
