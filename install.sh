e5a8ad1d9da714dbbe4e6ea2e5b4c5a62ef4b615
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-12 17:00:17 +0200

    selects current user as default user for this system automatically now

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "export USERNAME='$(whoami)'" > ~/.username
+
 sh ./install/xcode-dev-tools.sh && \
 sh ./install/brew.sh && \
 sh ./install/brew-cask.sh && \
 sh ./install/gem.sh && \
 sh ./install/yarn.sh && \
 sh ./install/emacs.sh && \
-sh ./install/dotfiles.sh &&\
+    sh ./install/dotfiles.sh
+
 chsh -s /bin/zsh
 
--- readme.md
+++ readme.md
@@ -4,10 +4,7 @@
 Clone this repository and execute
 
 ```sh
-
-echo "export USERNAME='your-user-name'" >> ~/.username
-
-./install.sh
+$ ./install.sh
 ```
 this should create symlinks to the files of this project.
 
