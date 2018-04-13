d2375911ecfc02e6669b58ea8f7be211bad95f68
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-13 10:38:27 +0200

    splitting languages from brew script

--- install/brew.sh
+++ install/brew.sh
@@ -73,13 +73,3 @@ apps=(
 brew install "${apps[@]}"
 brew cleanup
 
-# python versions
-pyenv install 3.6.4
-pyenv install 2.7
-pyenv global 2.6.4
-
-# ruby versions
-rbenv install 2.3.6
-rbenv install 2.4.3
-rbenv global 2.4.3
-
---
+++ install/languages.sh
@@ -0,0 +1,13 @@
+eval "$(rbenv init -)"
+eval "$(pyenv init -)"
+
+# python versions
+pyenv install 3.6.4
+pyenv install 2.7
+pyenv global 2.6.4
+
+# ruby versions
+rbenv install 2.3.6
+rbenv install 2.4.3
+rbenv global 2.4.3
+
