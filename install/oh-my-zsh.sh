83109e05c50b532d5ab467de652c7c5c4064f56e
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-07-24 16:16:51 +0200

    fixing zsh installation

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) && \
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
 rm -rf ~/.oh-my-zsh/custom/plugins && \
 ln -s ./oh-my-zsh/custom_plugins ~/.oh-my-zsh/custom/plugins
