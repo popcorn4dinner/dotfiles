b9ddc27d681aab677052b0a71f04adc4d5057373
Bastian Schnieders <bastian.schnieders@me.com>
2018-07-28 17:27:45 +0200

    fixed preferences

--- install/09_preferences.sh
+++ install/09_preferences.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env sh
 
 for source in preferences/*; do
-    target = "$HOME/Library/Preferences/$(basename $source)"
+    target="$HOME/Library/Preferences/$(basename $source)"
 
     if [ -f "$target" ]
     then
-        if ! [ -L target ]
+        if ! [ -L $target ]
         then
-            echo "creating backup of $target in .$target.bak"
-            mv ~/.$target ~/.$target.bak
-        else
-            echo "removing old link for $target"
-            rm $target
+            echo "creating backup of $target in $target.bak"
+            mv $target $target.bak
         fi
+    fi
+    if [ -L $target ]
+    then
+        echo "removing old link for $target"
+        rm $target
     fi
 
     echo "creating link for $target"
-    ln -s $source $target
+    ln -s "$(pwd)/$source" $target
 done
 echo "linked all preferences."
