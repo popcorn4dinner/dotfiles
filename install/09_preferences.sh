3ae11c24742d5ca2e3ebe9dd7936864d9519d927
Bastian Schnieders <bastian.schnieders@me.com>
2018-07-28 17:45:18 +0200

    new prefernce mechanism

--- install/09_preferences.sh
+++ install/09_preferences.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env sh
 
-for source in preferences/*; do
-    target="$HOME/Library/Preferences/$(basename $source)"
+echo "configuring iTerm2 settings"
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
-    if [ -f "$target" ]
-    then
-        if ! [ -L $target ]
-        then
-            echo "creating backup of $target in $target.bak"
-            mv $target $target.bak
-        fi
-    fi
-    if [ -L $target ]
-    then
-        echo "removing old link for $target"
-        rm $target
-    fi
 
-    echo "creating link for $target"
-    ln -s "$(pwd)/$source" $target
-done
-echo "linked all preferences."
