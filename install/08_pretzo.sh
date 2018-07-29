a40d789dd24a2a1f239178de627c0ef671602bb2
Bastian Schnieders <bastian.schnieders@me.com>
2018-07-29 13:17:54 +0200

    squash! work in progress

--- install/08_pretzo.sh
+++ install/08_pretzo.sh
@@ -7,5 +7,14 @@ for rcfile in “${ZDOTDIR:-$HOME}”/.zprezto/runcoms/^README.md(.N); do
     ln -s “$rcfile” “${ZDOTDIR:-$HOME}/.${rcfile:t}”
 done
 
+# install newest version of powerlevel9k prompt
+rm -rf ~/.zprezto/modules/prompt/external/powerlevel9k
+rm -f ~/.zprezto/modules/prompt/functions/prompt_powerlevel9k_setup
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.zprezto/modules/prompt/external/powerlevel9k
+
+ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/prompt_powerlevel9k_setup
+
+# make zsh default shell
 chsh -s /bin/zsh
 
