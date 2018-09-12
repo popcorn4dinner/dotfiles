c82d0346604d414b3fc71703a08cf36b7b92c1fb
Bastian Schnieders <bastian.schnieders@me.com>
2018-09-12 13:59:43 +0200

    fix for spacemacs setup

--- install/06_spacemacs.sh
+++ install/06_spacemacs.sh
@@ -11,3 +11,8 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # crystal layer
 git clone https://github.com/brantou/spacemacs-crystal-layer.git ~/.emacs.d/private/crystal
 
+# org-trello
+git clone https://github.com/popcorn4dinner/trello-org-spacemacs-layer.git ~/.emacs.d/private/trello-org
+
+# Create emacs link in applications folder
+ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications
