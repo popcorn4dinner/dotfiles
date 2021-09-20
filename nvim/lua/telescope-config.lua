8b3ce98ed304dbf5c0b8ad7d33fcfe535db3acd7
Bastian Schnieders <bastian.schnieders@me.com>
2021-09-20 19:39:05 +0200

    remove now obsolete files

--- nvim/lua/telescope-config.lua
+++
@@ -1,7 +0,0 @@
-require('telescope').setup{
-  defaults = {
-    color_devicons = false,
-    file_sorter = require'telescope.sorters'.get_fzy_sorter,
-    generic_sorter = require'telescope.sorters'.get_fzy_sorter
-  }
-}
